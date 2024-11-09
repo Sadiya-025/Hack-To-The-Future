ec29cc360f2efa794bb91b00ae6ba6c8683f988f
Sadiya-025 <adibasadiya9502@gmail.com>
2024-11-09 23:30:03 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,215 @@
+import random
+import pandas as pd
+import os, cohere, markdown, joblib
+from flask_sqlalchemy import SQLAlchemy
+from sklearn.preprocessing import LabelEncoder
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+
+db=SQLAlchemy(app)
+app.secret_key = os.getenv('FLASK_SECRET_KEY', 'fallback_secret_key')
+co = cohere.Client('9xtb0BFwgzrTgGQIG7QJrdZAUBH8oQoW30EK2Z7I')
+
+model = joblib.load('gut_health_model.pkl')
+scaler = joblib.load('scaler.pkl')
+feature_columns = joblib.load('feature_columns.pkl')
+encoder = LabelEncoder()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    date_joined = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+app.app_context().push()
+db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user=User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()        
+        return redirect(url_for('signin'))
+    
+    return render_template('sign-up.html')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Authenticates user
+        existing_user=User.query.filter_by(username=username, password=password).first()
+        if existing_user:
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('not-found.html')
+    
+    return render_template('sign-in.html')
+
+@app.route('/signout', methods=['GET', 'POST'])
+def signout():
+    # Clears the session
+    session.clear()
+    return redirect(url_for('signin'))
+
+@app.route('/about', methods=['GET'])
+def about():
+    if request.method == 'GET':
+        return render_template('about.html')
+    
+@app.route('/contact', methods=['GET'])
+def contact():
+    if request.method == 'GET':
+        return render_template('contact.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' not in session:
+        return redirect(url_for('signin'))
+    
+    username = session['username']
+    return render_template('dashboard.html', username=username)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'username' not in session:
+        return redirect(url_for('signin'))
+    
+    data = {
+        'Gender': [request.form['Gender']],
+        'Age': [float(request.form['Age'])],
+        'Height': [float(request.form['Height'])],
+        'Weight': [float(request.form['Weight'])],
+        'family_history_with_overweight': [request.form['family_history_with_overweight']],
+        'FAVC': [request.form['FAVC']],
+        'FCVC': [float(request.form['FCVC'])],
+        'NCP': [float(request.form['NCP'])],
+        'CAEC': [request.form['CAEC']],
+        'SMOKE': [request.form['SMOKE']],
+        'CH2O': [float(request.form['CH2O'])],
+        'SCC': [request.form['SCC']],
+        'FAF': [int(request.form['FAF'])],
+        'TUE': [int(request.form['TUE'])],
+        'CALC': [request.form['CALC']],
+        'MTRANS': [request.form['MTRANS']],
+        'NObeyesdad': [request.form['NObeyesdad']]
+    }
+
+    
+    # Creates a DataFrame from input
+    new_data = pd.DataFrame(data)
+
+    label_cols = ['Gender', 'family_history_with_overweight', 'FAVC', 'SMOKE', 'SCC']
+    for col in label_cols:
+        new_data[col] = encoder.fit_transform(new_data[col])
+
+    new_data = pd.get_dummies(new_data, columns=['MTRANS'], drop_first=False)
+    new_data = pd.get_dummies(new_data, columns=['CAEC'], drop_first=False)
+    new_data = pd.get_dummies(new_data, columns=['CALC'], drop_first=False)
+    new_data = pd.get_dummies(new_data, columns=['NObeyesdad'], drop_first=False)
+
+    missing_cols = set(feature_columns) - set(new_data.columns)
+    for col in missing_cols:
+        new_data[col] = 0
+
+    # Ensures column order matches the training data
+    new_data = new_data[feature_columns]
+
+    gut_health_columns = ['NObeyesdad_Insufficient_Weight', 'NObeyesdad_Normal_Weight', 'NObeyesdad_Obesity_Type_I', 'NObeyesdad_Obesity_Type_II', 'NObeyesdad_Obesity_Type_III', 'NObeyesdad_Overweight_Level_I', 'NObeyesdad_Overweight_Level_II', 'MTRANS_Automobile', 'MTRANS_Public_Transportation', 'MTRANS_Bike', 'MTRANS_Motorbike', 'MTRANS_Walking', 'CALC_Frequently', 'CALC_no', 'CALC_Sometimes', 'CAEC_Always', 'CAEC_Frequently', 'CAEC_no', 'CAEC_Sometimes']
+    new_data[gut_health_columns] = new_data[gut_health_columns].astype(int)
+
+    # Scales the features
+    new_data_scaled = scaler.transform(new_data)
+
+    y_pred_new = model.predict(new_data_scaled)
+
+    session['new_data'] = new_data.to_json()
+
+    # Outputs the predicted gut health
+    if y_pred_new[0][0] == 1:
+        result = "Good"
+    elif y_pred_new[0][1] == 1:
+        result = "Moderate"
+    else:
+        result = "Poor"
+
+    # Generates a personalized health report using Generative AI (Cohere)
+    prompt = f"""
+    Generate a personalized health report based on the following user data:
+    Gender: {request.form['Gender']}
+    Age: {request.form['Age']}
+    Height: {request.form['Height']} meters
+    Weight: {request.form['Weight']} kg
+    Family history of overweight: {request.form['family_history_with_overweight']}
+    Physical Activity Frequency: {request.form['FAF']} times per week
+    Diet-related habits (FAVC, FCVC, etc.): FAVC - {request.form['FAVC']}, FCVC - {request.form['FCVC']}
+    Smoking habit: {request.form['SMOKE']}
+    Daily water intake (CH2O): {request.form['CH2O']} liters
+    TUE (Technology usage time): {request.form['TUE']} hours
+
+    Provide a detailed health report with the following:
+    1. Evaluation of current health based on the user's data.
+    2. Personalized diet suggestions and exercise recommendations based on the age, weight, height, and activity levels.
+    3. Long-term health improvement tips based on the data (including managing weight, increasing physical activity, dietary improvements, and stress reduction).
+    
+    Restructure it with Bullet Points
+    """
+
+    response = co.generate(
+        model="command-r-plus-08-2024",
+        prompt=prompt,
+        max_tokens=1000
+    )
+
+    health_report = response.generations[0].text.strip()
+    health_report = markdown.markdown(health_report)
+    username = session['username']
+
+    return render_template('dashboard.html', prediction=result, health_report=health_report, username=username)
+
+@app.route('/inference', methods=['GET', 'POST'])
+def inference():
+    new_data_json = session.get('new_data')
+    new_data = pd.read_json(new_data_json)
+
+    trend_data = []  
+    new_data_copy = new_data.copy()  
+
+    for month in range(1, 13):  
+        new_data_copy['Age'] += 1/12  
+        new_data_copy['Weight'] += random.uniform(-0.1, 0.1)  # Minor Weight Fluctuations
+        new_data_copy['FCVC'] = new_data_copy['FCVC'].apply(lambda x: max(1, min(3, x + random.uniform(-0.1, 0.1))))
+        new_data_copy['FAF'] = new_data_copy['FAF'].apply(lambda x: max(0, x + random.choice([-1, 0, 1])))  # Activity Fluctuations
+        
+        new_data_scaled_copy = scaler.transform(new_data_copy) 
+
+        y_pred_new_copy = model.predict(new_data_scaled_copy)
+
+        if y_pred_new_copy[0][0] == 1:
+            risk_score = "Good"
+        elif y_pred_new_copy[0][1] == 1:
+            risk_score = "Moderate"
+        else:
+            risk_score = "Poor"
+        
+        trend_data.append({"month": month, "risk_score": risk_score})
+
+    return jsonify({"trend_data": trend_data})
+
+@app.route('/forecast', methods=['GET', 'POST'])
+def forecast():
+    return render_template("forecast.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/models.py
@@ -0,0 +1,166 @@
+import joblib
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+filename = 'train.csv'
+train = pd.read_csv(filename)
+train_data = train.drop('id', axis=1)
+
+def calculate_gut_health(row):
+    score = 0
+
+    # Gender
+    score += 1 if row['Gender'] == 'Female' else 0
+
+   # Age
+    if 18 <= row['Age'] <= 30:
+        score += 2
+    elif 31 <= row['Age'] <= 50:
+        score += 1
+    elif 51 <= row['Age'] <= 61:
+        score += 0
+    else:
+        score -= 1
+
+   # BMI (using Height and Weight to calculate BMI)
+    bmi = row['Weight'] / (row['Height'] ** 2)
+
+    # Scoring based on BMI range
+    if 18.5 <= bmi <= 24.9:
+        score += 2  # Normal weight
+    elif 25 <= bmi <= 29.9:
+        score += 1  # Overweight
+    elif 30 <= bmi <= 34.9:
+        score = 0  # Obesity class 1 (mild obesity)
+    elif 35 <= bmi <= 39.9:
+        score = -1  # Obesity class 2 (moderate obesity)
+    elif 40 <= bmi <= 79:
+        score = -2  # Obesity class 3 (severe obesity or extreme obesity)
+    else:
+        score = -3  # Extremely low BMI
+
+    # Family History with Overweight
+    score += 1 if row['family_history_with_overweight'] == 'no' else -1
+
+    # FAVC (Frequent High-Caloric Food Consumption)
+    score += 2 if row['FAVC'] == 'no' else -1
+
+    # FCVC (Frequency of Vegetable Consumption)
+    if row['FCVC'] >= 2.5:
+        score += 2
+    elif 1.5 <= row['FCVC'] < 2.5:
+        score += 1
+    else:
+        score -= 1
+
+    # NCP (Number of Meals per Day)
+    if 3 <= row['NCP'] <= 4:
+        score += 2
+    elif 2 <= row['NCP'] < 3:
+        score += 1
+    else:
+        score -= 1
+
+    # CAEC (Consumption Between Meals)
+    if row['CAEC'] == 'no':
+        score += 2
+    elif row['CAEC'] == 'Sometimes':
+        score += 1
+    elif row['CAEC'] == 'Frequently':
+        score -= 1
+    else:
+        score -= 2
+
+    # SMOKE
+    score += 2 if row['SMOKE'] == 'no' else -2
+
+    # CH2O (Daily Water Intake)
+    score += 2 if row['CH2O'] >= 2.5 else (1 if row['CH2O'] >= 2.0 else -1)
+
+    # SCC (Self-Monitoring Calories)
+    score += 1 if row['SCC'] == 'yes' else 0
+
+    # FAF (Physical Activity Frequency)
+    score += 2 if row['FAF'] >= 2.5 else (1 if row['FAF'] >= 1.0 else -1)
+
+    # TUE (Technology Usage)
+    score += 2 if row['TUE'] <= 1 else (1 if row['TUE'] <= 1.5 else -1)
+
+    # CALC (Alcohol Consumption)
+    score += 2 if row['CALC'] == 'no' else (-1 if row['CALC'] == 'Frequently' else 1)
+
+    # MTRANS (Transportation Mode)
+    score += 2 if row['MTRANS'] == 'Walking' else (1 if row['MTRANS'] == 'Bike' or row['MTRANS'] == 'Public_Transportation' else 0)
+
+    # NObeyesdad (Obesity Classification)
+    score += 2 if row['NObeyesdad'] == 'Normal_Weight' else (-1 if row['NObeyesdad'].startswith('Obesity') else 1)
+
+    if score >= 15:
+        return 'Good'
+    elif score >= 0:
+        return 'Moderate'
+    else:
+        return 'Poor'
+
+# Applys the function to create the 'Gut Health' column
+train_data['Gut_Health'] = train_data.apply(calculate_gut_health, axis=1)
+data = train_data
+
+# Handles categorical variables
+label_cols = ['Gender', 'family_history_with_overweight', 'FAVC', 'SMOKE', 'SCC']  # Add all categorical columns here
+encoder = LabelEncoder()
+for col in label_cols:
+    data[col] = encoder.fit_transform(data[col])
+
+# One-hot encoding for multi-class columns (e.g., 'MTRANS' with multiple categories)
+data = pd.get_dummies(data, columns=['MTRANS'], drop_first=False)
+data = pd.get_dummies(data, columns=['CAEC'], drop_first=False)
+data = pd.get_dummies(data, columns=['CALC'], drop_first=False)
+data = pd.get_dummies(data, columns=['NObeyesdad'], drop_first=False)
+data = pd.get_dummies(data, columns=['Gut_Health'], drop_first=False)
+
+gut_health_columns = ['Gut_Health_Good', 'Gut_Health_Moderate', 'Gut_Health_Poor', 'NObeyesdad_Insufficient_Weight', 'NObeyesdad_Normal_Weight', 'NObeyesdad_Obesity_Type_I', 'NObeyesdad_Obesity_Type_II', 'NObeyesdad_Obesity_Type_III', 'NObeyesdad_Overweight_Level_I', 'NObeyesdad_Overweight_Level_II', 'MTRANS_Automobile', 'MTRANS_Public_Transportation', 'MTRANS_Bike', 'MTRANS_Motorbike', 'MTRANS_Walking', 'CALC_Frequently', 'CALC_no', 'CALC_Sometimes', 'CAEC_Always', 'CAEC_Frequently', 'CAEC_no', 'CAEC_Sometimes']
+data[gut_health_columns] = data[gut_health_columns].astype(int)
+
+# Separates features (X) and target (y)
+X = data.drop(columns=['Gut_Health_Good', 'Gut_Health_Moderate', 'Gut_Health_Poor'])
+y = data[['Gut_Health_Good', 'Gut_Health_Moderate', 'Gut_Health_Poor']]
+
+# Standardizes the features
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Splits the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+# Trains a Random Forest Classifier
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+# Makes predictions
+y_pred = model.predict(X_test)
+
+y_test_np = y_test.to_numpy()
+y_pred_np = y_pred
+
+# Evaluates the model
+accuracy = accuracy_score(y_test_np, y_pred_np)
+conf_matrix = confusion_matrix(y_test_np.argmax(axis=1), y_pred_np.argmax(axis=1))
+class_report = classification_report(y_test_np.argmax(axis=1), y_pred_np.argmax(axis=1))
+
+# Outputs results
+print(f'Accuracy: {accuracy}')
+print(f'Confusion Matrix:\n{conf_matrix}')
+print(f'Classification Report:\n{class_report}')
+
+# Saves the model
+joblib.dump(model, 'gut_health_model.pkl')
+
+# Saves the scaler
+joblib.dump(scaler, 'scaler.pkl')
+
+feature_columns = X.columns  # Column names from training data
+joblib.dump(feature_columns, 'feature_columns.pkl')
